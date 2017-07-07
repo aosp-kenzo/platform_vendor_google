fd88de90b1bc4983245164b7c2d0d0187adb07cd
doveki <2290218900@qq.com>
2017-07-07 09:21:56 +0800

    update

--- products/gms.mk
+++ products/gms.mk
@@ -1,9 +1,19 @@
-# vendor path
-VENDOR_PATH := vendor/google
+# Copyright (C) 2016 The Sony AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Include overlays
-PRODUCT_PACKAGE_OVERLAYS += \
-    $(VENDOR_PATH)/overlay/common
+# vendor path
+VENDOR_AOSP_PATH := vendor/google
 
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -16,11 +26,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.setupwizard.rotation_locked=true
 
+# Include overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    $(VENDOR_AOSP_PATH)/overlay/common
+
 # OpenGapps
 GAPPS_VARIANT := mini
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_WEBVIEW_OVERRIDES := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
+GAPPS_FORCE_MMS_OVERRIDES := true
+GAPPS_FORCE_DIALER_OVERRIDES := true
 
 PRODUCT_PACKAGES += \
      GoogleCamera \
@@ -37,5 +53,9 @@ GAPPS_EXCLUDED_PACKAGES += \
 # Google Assistant
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opa.eligible_device=true
+
+# bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/google/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 $(call inherit-product, vendor/google/build/opengapps-packages.mk)
