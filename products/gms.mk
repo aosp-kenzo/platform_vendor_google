2013e553bbc1fed2fc9163360daa6616acf48b98
doveki <2290218900@qq.com>
2017-08-03 04:09:59 +0800

    Update gms.mk

--- products/gms.mk
+++ products/gms.mk
@@ -15,18 +15,6 @@
 # vendor path
 VENDOR_SONYAOSP_PATH := vendor/google
 
-# Google property overides
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
-    ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.error.receiver.system.apps=com.google.android.gms \
-    ro.setupwizard.enterprise_mode=0 \
-    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
-    ro.setupwizard.rotation_locked=true \
-    ro.setupwizard.mode=DISABLED
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
@@ -43,7 +31,17 @@ PRODUCT_COPY_FILES +=  \
     vendor/google/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 # OpenGapps
+# Google property overides
 ifeq ($(WITH_GAPPS),true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.setupwizard.rotation_locked=true
 GAPPS_VARIANT := mini
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_WEBVIEW_OVERRIDES := true
