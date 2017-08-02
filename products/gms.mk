f046a0e419e1a671103c59481d0e3deab107733a
doveki <2290218900@qq.com>
2017-08-03 07:24:16 +0800

    Update gms.mk

--- products/gms.mk
+++ products/gms.mk
@@ -31,8 +31,8 @@ PRODUCT_COPY_FILES +=  \
     vendor/google/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 # OpenGapps
-# Google property overides
 ifeq ($(WITH_GAPPS),true)
+# Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
@@ -41,7 +41,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
-    ro.setupwizard.rotation_locked=true
+    ro.setupwizard.rotation_locked=
+
 GAPPS_VARIANT := mini
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_WEBVIEW_OVERRIDES := true
@@ -61,19 +62,30 @@ GAPPS_EXCLUDED_PACKAGES += \
     Hangouts \
     CalculatorGoogle \
     CalendarGooglePrebuilt \
-    PrebuiltDeskClockGoogle
-$(call inherit-product, vendor/google/build/opengapps-packages.mk)
-endif
-
-# Telephony Packages
-ifeq ($(WITH_PIXEL),true)
-GAPPS_FORCE_DIALER_OVERRIDES := true
-GAPPS_FORCE_MMS_OVERRIDES := true
-GAPPS_FORCE_PIXEL_LAUNCHER := true
-PRODUCT_PACKAGES += \
-    GoogleContacts 
-endif
+    PrebuiltDeskClockGoogle \
+    LatinIME \
+    PackageInstaller \
+    Camera2 \
+    Launcher3
 
 # Google Assistant
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opa.eligible_device=true
+
+$(call inherit-product, vendor/google/build/opengapps-packages.mk)
+endif
+
+ifeq ($(WITH_PIXEL),true)
+# Telephony Packages
+GAPPS_FORCE_DIALER_OVERRIDES := true
+GAPPS_FORCE_MMS_OVERRIDES := true
+GAPPS_FORCE_PIXEL_LAUNCHER := true
+
+PRODUCT_PACKAGES += \
+    GoogleContacts 
+
+GAPPS_EXCLUDED_PACKAGES += \
+    Dialer \
+    Contacts \
+    Messaging
+endif
