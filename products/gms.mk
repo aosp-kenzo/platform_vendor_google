6902baf7f2f89ba607ab04b11587a9ca44f42477
doveki <2290218900@qq.com>
2017-08-11 21:25:21 +0800

    Clean and Update

--- products/gms.mk
+++ products/gms.mk
@@ -1,3 +1,4 @@
+
 # Copyright (C) 2016 The Sony AOSP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +16,23 @@
 # vendor path
 VENDOR_SONYAOSP_PATH := vendor/google
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Include overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    $(VENDOR_SONYAOSP_PATH)/overlay/common
+
+# bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/google/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.maintainer=$(shell users)
+
+# OpenGapps
+ifeq ($(WITH_GAPPS),true)
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
@@ -22,109 +40,54 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.error.receiver.system.apps=com.google.android.gms \
-    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.enterprise_mode=0 \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
-    ro.setupwizard.rotation_locked=true
+    ro.setupwizard.rotation_locked=true \
+    ro.setupwizard.mode=DISABLED \
+    ro.com.google.ime.theme_id=5
 
-# SELinux
-BOARD_USE_ENFORCING_SELINUX := true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
-
-# Kernel-headers FLAG
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-
-# Include overlays
-PRODUCT_PACKAGE_OVERLAYS += \
-    $(VENDOR_SONYAOSP_PATH)/overlay/common
-
-# Night Mode
-#ifneq ($(filter-out aosp_f813% aosp_f833% aosp_g823% aosp_g814%, $(TARGET_PRODUCT)),)
-PRODUCT_PACKAGE_OVERLAYS += \
-    $(VENDOR_SONYAOSP_PATH)/overlay-night/common
-#endif
-
-# Audio (Notifications/Alarms)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Tethys.ogg \
-    ro.config.alarm_alert=Oxygen.ogg
-
-# libfuse
-PRODUCT_PACKAGES += \
-    libfuse
-
-# exfat
-PRODUCT_PACKAGES += \
-    fsck.exfat \
-    libexfat \
-    mkfs.exfat \
-    mount.exfat
-
-# Audio (Ringtones - Not windy devices allowed)
-ifneq ($(filter-out aosp_sgp511 aosp_sgp611 aosp_sgp712, $(TARGET_PRODUCT)),)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Titania.ogg
-endif
-
-# bootanimation (240-320 DPI)
-ifneq ($(filter aosp_sgp521 aosp_sgp511_windy aosp_sgp621 aosp_sgp611_windy aosp_d5803 aosp_e23% aosp_e5823 aosp_d5503 aosp_c6833 aosp_sgp771 aosp_sgp712_windy aosp_f5321, $(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES +=  \
-    vendor/google/prebuilt/common/bootanimation/240_320/bootanimation.zip:system/media/bootanimation.zip
-endif
-
-# bootanimation (480 DPI)
-ifneq ($(filter aosp_c6903 aosp_d6503 aosp_d6603 aosp_e65% aosp_e66% aosp_e68% aosp_f512% aosp_f813% aosp_f833% aosp_g823% aosp_g814%, $(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES +=  \
-    vendor/google/prebuilt/common/bootanimation/480/bootanimation.zip:system/media/bootanimation.zip
-endif
-
-# RIL
-ifneq ($(filter aosp_c6903 aosp_d5503 aosp_c6833, $(TARGET_PRODUCT)),)
-BOARD_RIL_CLASS := ../../../$(VENDOR_SONYAOSP_PATH)/ril-rhine/
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SonyRIL
-endif
-
-# OpenGapps
-GAPPS_VARIANT := mini
+GAPPS_VARIANT := micro
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_WEBVIEW_OVERRIDES := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
-GAPPS_FORCE_PIXEL_LAUNCHER := true
+
+# Add some extras not in micro
+# To override stock AOSP apps
+PRODUCT_PACKAGES += \
+    LatinImeGoogle \
+    GoogleCamera \
+    Photos
+
+GAPPS_EXCLUDED_PACKAGES += \
+    PrebuiltGmail \
+    PrebuiltExchange3Google \
+    LatinIME \
+    PackageInstaller \
+    Camera2 \
+    Launcher3 \
+    Gallery2 \
+    Browser \
+    CalendarGooglePrebuilt
 
 # Google Assistant
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opa.eligible_device=true
 
-# Telephony Packages (Not windy devices allowed)
-ifneq ($(filter-out aosp_sgp511 aosp_sgp611 aosp_sgp712, $(TARGET_PRODUCT)),)
+$(call inherit-product, vendor/google/build/opengapps-packages.mk)
+endif
+
+ifeq ($(WITH_PIXEL),true)
+# Telephony Packages
 GAPPS_FORCE_DIALER_OVERRIDES := true
 GAPPS_FORCE_MMS_OVERRIDES := true
+GAPPS_FORCE_PIXEL_LAUNCHER := true
 
-# Audio (Ringtones)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Orion.ogg
-endif
-
-# Add some extras not in micro
-# To override stock AOSP apps
 PRODUCT_PACKAGES += \
     GoogleContacts \
-    LatinImeGoogle \
-    Music2
+    PrebuiltDeskClockGoogle
 
-# GoogleCamera doesn't support HAL1 on Nougat
-# Allow only Tone and Yoshino for now
-ifneq ($(filter aosp_f813% aosp_f833% aosp_g823% aosp_g814%, $(TARGET_PRODUCT)),)
-PRODUCT_PACKAGES += \
-    GoogleCamera
+GAPPS_EXCLUDED_PACKAGES += \
+    Dialer \
+    Contacts \
+    Mms
 endif
-
-ifneq ($(filter-out aosp_c6903 aosp_c6833 aosp_d5503, $(TARGET_PRODUCT)),)
-PRODUCT_PACKAGES += \
-    TagGoogle
-endif
-
-$(call inherit-product, vendor/google/build/opengapps-packages.mk)
